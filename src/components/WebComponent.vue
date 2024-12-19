67f611932e5409d8e2e3df2efbd7d77d49042695
knm8643 <knm8643@nate.com>
2024-12-19 23:38:03 +0900

    12/19 저녁

--- src/components/WebComponent.vue
+++ src/components/WebComponent.vue
@@ -9,16 +9,14 @@
       </div>
 
       <div class="web-content-two">
-        <div class="content-kakao">
-          <!--
-          <i></i>
-          -->
-          <p>문의하기</p>
+        <div class="content-git">
+          <img src="../assets/img/icon/github_icon.png" alt="github">
+          <div class="git-info">
+            <p @click="sendUrlGit('made')">개발자 알아보기</p>
+            <span @click="sendUrlGit('help')">도움준 사람</span>
+          </div>
         </div>
-        <div class="content-git" @click="sendGit">
-          <!--
-          <i></i>
-          -->
+        <div class="content-other">
           <p>공지사항</p>
         </div>
       </div>
@@ -109,6 +107,16 @@ export default {
       return true;
     },
 
+    // 단순페이지 전송
+    sendUrlGit(param) {
+      // 깃허브 gh특성상 새창으로
+      if (param === 'made') {
+        window.open('https://github.com/knm8643', '_blank');
+      } else {
+        window.open('https://github.com/yu-rim95', '_blank');
+      }
+    },
+
     // 가입신청으로 화면전환
     changeTextLogin() {
       this.isLogin = !this.isLogin;
@@ -127,19 +135,24 @@ export default {
 
     sendInfo() {
       if(this.vailCheck()) {
-        emailjs.init("HsS2kuozk9ToWT7O7");
-        emailjs
-            .send("service_portfolio", "template_1mkalef", {
-              to_name: 'chungrimi.com',
-              from_name: 'chungrimi.com',
-              message: this.userEmail,
-            })
-            .then(() => {
-              alert("회원가입 신청이 성공했습니다. \n(승인까지 약 1일 내외 소요)");
-            })
-            .catch(() => {
-              alert("API 통신에러");
-            });
+        const emailExists = this.userInfos.some(userInfo => userInfo.name === this.userEmail);
+        if (!emailExists) {
+          emailjs.init("HsS2kuozk9ToWT7O7");
+          emailjs
+              .send("service_portfolio", "template_1mkalef", {
+                to_name: 'chungrimi.com',
+                from_name: 'chungrimi.com',
+                message: this.userEmail,
+              })
+              .then(() => {
+                alert("회원가입 신청이 성공했습니다. \n(승인까지 약 1일 내외 소요)");
+              })
+              .catch(() => {
+                alert("API 통신에러");
+              });
+        } else {
+          alert("이미 가입신청된 상태입니다.")
+        }
       }
     },
 
@@ -187,39 +200,48 @@ export default {
       display: flex;
       gap: 24px;
 
-      .content-kakao{
-        i {
-          width: 32px;
-          height: 100%;
-          background-image: url('../assets/img/icon/kakao_icon.svg');
-          background-repeat: no-repeat;
-          background-size: contain;
-          display: inline-block;
+      .content-git{
+        img{
+          border-radius: 50%;
+          width: 42px;
+          height: 42px;
         }
-        p{
-          text-align: center;
-          font-weight: 700;
-          font-size: 14px;
+
+        .git-info{
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          p{
+            font-weight: 700;
+            font-size: 14px;
+          }
+          span{
+            padding-top: 4px;
+            font-weight: 500;
+            color: #757575;
+            font-size: 14px;
+          }
+
+          p:hover {
+            color: #fa8ea8;
+          }
+
+          span:hover{
+            font-weight: 700;
+          }
         }
+
         width: 50%;
         gap: 12px;
         display: flex;
         align-items: center;
         justify-content: center;
         border-radius: 6px;
-        padding: 6px 21px;
+        padding: 24px 21px;
         background: #ffffff;
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.06);
       }
-      .content-git{
-        i {
-          width: 32px;
-          height: 100%;
-          background-image: url('../assets/img/icon/kakao_icon.svg');
-          background-repeat: no-repeat;
-          background-size: contain;
-          display: inline-block;
-        }
+      .content-other{
         p{
           text-align: center;
           font-weight: 700;
@@ -261,10 +283,11 @@ export default {
             max-width: 345px;
             width: 100%;
             padding: 0 0 0 12px;
-            border: 0.6px solid #757575;
+            border: 0.6px solid #b7b7b7;
             transition: border-color 0.3s ease, background-color 0.3s ease;
 
             input {
+              font-size: 14px;
               width: 100%;
             }
 
